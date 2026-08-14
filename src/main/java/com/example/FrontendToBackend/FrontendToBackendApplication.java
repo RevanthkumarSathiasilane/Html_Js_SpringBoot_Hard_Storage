bef0769a570ca1175004ae25a05443b7f054fb36
RevanthkumarSathiasilane <revanthkumarsathiasilane2908@gmail.com>
2026-08-14 20:07:46 +0530

    Started My First Backend Code

---
+++ src/main/java/com/example/FrontendToBackend/FrontendToBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.FrontendToBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendToBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendToBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FrontendToBackend/FrontendToBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FrontendToBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendToBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
