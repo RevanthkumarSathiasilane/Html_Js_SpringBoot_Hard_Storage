dfc14455e6300800bf1da31e2ea905f59278aa12
RevanthkumarSathiasilane <revanthkumarsathiasilane2908@gmail.com>
2026-08-14 21:07:00 +0530

    Get method finished and Post method Started

--- src/main/java/com/example/FrontendToBackend/controller/StudentController.java
+++ src/main/java/com/example/FrontendToBackend/controller/StudentController.java
@@ -5,10 +5,11 @@ import com.example.FrontendToBackend.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5500")
 public class StudentController {
     @Autowired
     StudentService studentService;
